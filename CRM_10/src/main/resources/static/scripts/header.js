0e96008f669f8be5007130c40106480c9b9b0f17
Toucan_TI <mauro.matheus77@gmail.com>
2026-02-05 12:51:24 -0300

    excluir sapoha

--- CRM_10/src/main/resources/static/scripts/header.js
+++
@@ -1,8 +0,0 @@
-//animando um botão em js só pra testar minhas capacidades. como front-end.
-
-let headerButtons = window.document.querySelectorAll(".header-button");
-
-headerButtons.forEach((headerBtn) => {
-  addEventListener("mouseneter", subir);
-  addEventListener("mouseleave", descer);
-});
